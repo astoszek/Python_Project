260f01114532ad31f9b36ad560b8d828711dca6b
stoszek <a.stoszek@gmail.com>
2024-10-06 15:15:02 +0200

    commit

--- pp6/number.py
+++ pp6/number.py
@@ -1,3 +1,3 @@
 string_number = input("Podaj liczbę całkowitą: ")
 multiplier = 9
-print("Gdy liczbę", string_number + "pomnożymy przez " + str(multiplier) + " to otrzymamy " + str(multiplier * int(string_number)) + ".")
+print("Gdy liczbę", string_number + " pomnożymy przez " + str(multiplier) + " to otrzymamy " + str(multiplier * int(string_number)) + ".")
---
+++ pp6/pythagorean.py
@@ -0,0 +1,5 @@
+# twierdzenie Pitagorasa
+a = float(input("Wprowadź długośc pierwszego boku: "))
+b = float(input("Wprowadź długośc drugiego boku: "))
+
+print("Długość przeciwprostokątnej wynosi " + str((a ** 2 + b ** 2) ** .5))3
