c36d0b48d2fd65dc6c82744549a05d3753a83d4f
Stosiu <a.stoszek@gmail.com>
2024-12-08 16:14:42 +0100

    ok

--- pp16/deom.py
+++ pp16/deom.py
@@ -1,12 +1,39 @@
-# import math
-# import sys
+# # # # import math
+# # # # import sys
+# # # #
+# # # # print(math.pi)
+# # # # print(math.sin(math.pi / 2))
+# # # # print(math.factorial(5))
+# # #
+# # # # import math as m
+# # # # print(m.pi)
+# # # #
+# # # # from math import pi as PI
+# # # # print(PI)
+# # #
+# # # import random
+# # #
+# # # print(random.random())
+# #
+# # from random import random, seed
+# #
+# # seed(0)
+# #
+# # for i in range(50):
+# #     print(random())
 #
-# print(math.pi)
-# print(math.sin(math.pi / 2))
-# print(math.factorial(5))
+# from random import choice, sample
+# lst = [i for i in range(1, 11)]
+#
+# print(choice(lst))#wybiera jedna liczbe ze zbioru
+# print(sample(lst, 5))#wybiera  5 liczb ze zbioru bez powtózeń
 
-# import math as m
-# print(m.pi)
 
-from math import pi as PI
-print(PI)
+import platform
+
+
+print(platform.machine())
+print(platform.version())
+print(platform.system())
+print(platform.python_implementation())
+print(platform.python_version_tuple())
