4ffb7fe04da05d47f29cdc1877817fb09f6beb61
Stosiu <a.stoszek@gmail.com>
2025-01-17 19:16:57 +0100

    ok

--- pp10/sum_all_elements.py
+++ pp10/sum_all_elements.py
@@ -2,8 +2,8 @@ total = 0
 
 numbers = [1, 3, 55, 3, 2, 5, 71, 12, 33]
 
-for number in numbers:
-    total += number
+for i in numbers:
+    total += i
 
 print("Suma wszystkich elementów listy" , numbers, "to", str(total) + ".")
 
