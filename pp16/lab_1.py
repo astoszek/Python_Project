c196174c1344270936b3b5329e275fec57db52b5
stoszek <a.stoszek@gmail.com>
2025-01-18 10:53:19 +0100

    commit

--- pp16/lab_1.py
+++ pp16/lab_1.py
@@ -1,3 +1,23 @@
 import platform
-print(platform.machine())
+import math
+from random import sample
 
+
+def draw_numbers():
+    numbers = [i for i in range(1, 11)]
+    unique_numbers = sample(numbers, 3)
+    return unique_numbers
+
+
+def machine():
+    return (platform.machine())
+
+
+def sinus():
+    print(math.sin(90))
+
+
+if __name__ == '__main__':
+    print(draw_numbers())
+    print(machine())
+    print(sinus())
